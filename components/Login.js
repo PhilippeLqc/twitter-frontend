e989c8f987e455248c9106f5fc439b7c4f3cec55
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-04-27 12:12:25 +0200

    frontLogin

---
+++ components/Login.js
@@ -0,0 +1,30 @@
+import styles from "../styles/Login.module.css";
+
+function Home() {
+  return (
+    <div>
+      <main className={styles.main}>
+
+        <div className={styles.title}>
+        <img src="/images/twitter-logo.png" alt="logo" className={styles.logoleft}/>
+        </div>
+
+        <div className={styles.right}>
+          <img src="/images/twitter-logo.png" alt="logo" className={styles.logo}/>
+          <p className={styles.text + ' ' + styles.toptext}>See what's happening</p>
+          <p className={styles.text}>Join Hackatweet today</p>
+          <button id="signup" className={styles.signup}>
+            Sign up
+          </button>
+          <p className={styles.text}>Already have an account ?</p>
+          <button id="signin" className={styles.signin}>
+            Sign in
+          </button>
+        </div>
+
+      </main>
+    </div>
+  );
+}
+
+export default Home;
