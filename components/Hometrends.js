fb7dc0ab11f2246f4314772f2c9b3d2958da8e43
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-04-28 15:32:42 +0200

    trends

--- components/Hometrends.js
+++ components/Hometrends.js
@@ -8,21 +8,25 @@ import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../reducers/user";
 
-function Home() {
-  const router = useRouter();
-  const [inputTweet, setInputTweet] = useState("");
+function Hometrends() {
   const [username, setusername] = useState("");
   const [firstname, setFirstname] = useState("");
+  const [inputTweet, setInputTweet] = useState("");
+  const [hashtags, sethashtags] = useState([]);
   const [id, setId] = useState("");
-  const [tweets, setTweets] = useState([]);
-  const [newTweet, setNewTweet] = useState(false);
-  const dispatch = useDispatch();
-
-  const Isdeleted = useSelector((state) => state.deleted.value);
-  const isLiked = useSelector((state) => state.liked.value);
   const user = useSelector((state) => state.user.value);
 
-  // récupère les infos du user et les stocks dans des variables (username, firstname, id)
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleHome = () => {
+    router.push("/homepage");
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    router.push("/login");
+  };
 
   useEffect(() => {
     fetch("http://localhost:3000/users/getUser", {
@@ -40,77 +44,17 @@ function Home() {
       });
   }, []);
 
-  // récupère les tweets et les stocks dans la const tweets
 
   useEffect(() => {
-    fetch("http://localhost:3000/tweets/getTweets")
+    fetch("http://localhost:3000/hashtags/getHashtags")
       .then((response) => response.json())
       .then((data) => {
-        setTweets(data);
+        console.log('data', data);
+        sethashtags(data);
       });
-  }, [newTweet, Isdeleted, isLiked]);
+  }, []);
 
-  // Au clic, ajoute un tweet en bdd
-
-  function handleTweet() {
-    const date = new Date();
-
-    const regex = new RegExp("#\\w+", "g");
-
-    const hashtagRegex = inputTweet.match(regex);
-
-    const hashtag = hashtagRegex[0];
-
-    console.log(hashtag);
-
-        fetch("http://localhost:3000/tweets/add", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            date: date,
-            message: inputTweet,
-            user: id,
-            hashtags: hashtag,
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-
-            fetch("http://localhost:3000/hashtags/add", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-              hashtag: hashtag,
-              tweetId: data.newDoc._id
-              }),
-            })
-              .then((response) => response.json())
-            setInputTweet("");
-            setNewTweet(!newTweet);
-          });
-      }
-  
-
-  // Map sur les tweets et les ajoutes dans la constante tweet
-
-  const tweet = tweets.map((data, i) => {
-    return <Tweet key={i} {...data} />;
-  });
-
-  // Bouton logout / déconnexion
-
-  const handleHome = () => {
-    router.push("/homepage");
-  };
-
-  const handleLogout = () => {
-    dispatch(logout());
-    router.push("/login");
-  };
-
-  const handleTrends = () => {
-    
-  }
+  console.log('hashtags', hashtags);
 
 
   return (
@@ -146,30 +90,20 @@ function Home() {
       <div className={styles.content_middle}>
         <div className={styles.home_container}>
           <div className={styles.title_middle_container}>
-            <p className={styles.title_middle}>Home</p>
+            <p className={styles.title_middle}>Trends</p>
           </div>
           <div className={styles.input_container}>
             <input
               className={styles.input_tweet}
               type="text"
-              placeholder="Whats'up?"
+              placeholder="research"
               id="signUpUsername"
               onChange={(e) => setInputTweet(e.target.value)}
               value={inputTweet}
             />
           </div>
-          <div className={styles.home_bottom}>
-            <p>{inputTweet.length}/280</p>
-            <button
-              className={styles.btn_tweet}
-              onClick={() => handleTweet()}
-              id="tweet"
-            >
-              Tweet
-            </button>
-          </div>
         </div>
-        <div className={styles.tweets_container}> {tweet}</div>
+        <div className={styles.tweets_container}> AFFICHER TWEETTRENDS</div>
       </div>
       <div className={styles.content_right}>
         <p className={styles.trends}>Trends</p>
@@ -181,4 +115,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Hometrends;
---
+++ pages/hometrends.js
@@ -0,0 +1,7 @@
+import Hometrends from '../components/Hometrends';
+
+function Hometrendspage () {
+  return <Hometrends />;
+}
+
+export default Hometrendspage;
